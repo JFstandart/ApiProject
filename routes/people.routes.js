aea6bebd6e9566e72f608a234f4534f84976bcf1
Juan Felipe Uribe Cardona <Juanfelipe.u.c7@gmail.com>
2018-08-06 07:38:00 -0500

    Update people.routes.js

--- routes/people.routes.js
+++ routes/people.routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const router = express.Router()
+const people = express.Router()
 const peopleController = require('../controllers/people.controller')
 
-router.get('people/', peopleController.getAllPeople)
-router.post('people/', peopleController.createPeople)
-router.get('people/:id', peopleController.getPeople)
-router.put('people/:id', peopleController.updatePeople)
-router.delete('people/:id', peopleController.deletePeople)
+people.get('/', peopleController.getAllPeople)
+people.post('/', peopleController.createPeople)
+people.get('/:id', peopleController.getPeople)
+people.put('/:id', peopleController.updatePeople)
+people.delete('/:id', peopleController.deletePeople)
 
-module.exports = router
+module.exports = people
